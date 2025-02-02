f99fca6d7c8925d21cc969c44053d808f9b498ac
Crackvignoule <kiki.kalagan@gmail.com>
2025-02-02 01:45:16 +0100

    Automatic Deployment

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
       <div>
-        <h1 className="text-4xl font-bold">Welcome to My App</h1>
-        <p className="mt-4">This is the home page.</p>
+        <h1 className="text-4xl font-bold">Welcome to Trade of Exile</h1>
+        <p className="mt-4">WIP</p>
       </div>
     )
   }
