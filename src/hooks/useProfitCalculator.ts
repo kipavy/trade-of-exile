f54c335664d9dcfd7ff529fc9777f5cd69c2dc24
Crackvignoule <kiki.kalagan@gmail.com>
2025-02-03 22:52:26 +0100

    fix routes

--- src/hooks/useProfitCalculator.ts
+++ src/hooks/useProfitCalculator.ts
@@ -1,22 +1,13 @@
 import { useState, useEffect } from 'react';
 import { calculateProfit } from '@/utils/calculateProfit';
-import useLocalStorage from '@/hooks/useLocalStorage';
-
-interface Amounts {
-  amount1: string | number;
-  amount2: string | number;
-}
+import { Amounts } from "@/types/profit"
 
 export default function useProfitCalculator(buy: Amounts, sell: Amounts): number | null {
   const [profit, setProfit] = useState<number | null>(null);
-  const [, setBuyAmounts] = useLocalStorage<Amounts>('buyAmounts', buy);
-  const [, setSellAmounts] = useLocalStorage<Amounts>('sellAmounts', sell);
 
   useEffect(() => {
     setProfit(calculateProfit(buy, sell));
-    setBuyAmounts(buy);
-    setSellAmounts(sell);
-  }, [buy, sell, setBuyAmounts, setSellAmounts]);
+  }, [buy, sell]);
 
   return profit;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter as Router } from "react-router-dom";  // Used to fix GitHub Pages routing instead of BrowserRouter
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </Router>
+    </BrowserRouter>
   </StrictMode>,
 )
