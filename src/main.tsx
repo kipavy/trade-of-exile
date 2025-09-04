301587c31278e2f42ffd241213e2fb8cdf7d5072
Crackvignoule <kiki.kalagan@gmail.com>
2025-09-05 01:18:50 +0200

    trying to fix github pages routing with hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById('root')!;
+
+createRoot(rootEl).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
